107b26070304164712a5ec3848289f5f2d75dc7a
SeoBamm <spot3161@gmail.com>
2024-04-26 22:06:13 +0900

    Initial Commit

--- WebSocketTest_1/src/main/java/com/termproject/websockettest_1/WebSocketTest1Application.java
+++ WebSocketTest_1/src/main/java/com/termproject/websockettest_1/WebSocketTest1Application.java
@@ -1,13 +1,13 @@
-package com.termproject.serertest;
+package com.termproject.websockettest_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SererTestApplication {
+public class WebSocketTest1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SererTestApplication.class, args);
+        SpringApplication.run(WebSocketTest1Application.class, args);
     }
 
 }
--- WebSocketTest_1/src/test/java/com/termproject/websockettest_1/WebSocketTest1ApplicationTests.java
+++ WebSocketTest_1/src/test/java/com/termproject/websockettest_1/WebSocketTest1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.termproject.serertest;
+package com.termproject.websockettest_1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SererTestApplicationTests {
+class WebSocketTest1ApplicationTests {
 
     @Test
     void contextLoads() {
